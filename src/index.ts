f7658446aafa665e23c2b9698fe06fb85e662d7a
Pavel Stančík <stancik@infowebsro.cz>
2021-05-10 09:16:06 +0200

    Masking ENV params as seecrets

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+
+const KEY_VAULT_URI = core.getInput('KEY_VAULT_URI');
+process.env['KEY_VAULT_URI'] = KEY_VAULT_URI;
+
 import * as manager from "enhanced-env-azure-vault";
 
 const underscoreReplacedBy = "0x";
@@ -25,14 +29,15 @@ const preparation = async (proposedEnvironment: string, proposedType: string ) =
 
   azureParameters.map( secretObject => {
     if (secretObject.enabled && secretObject.environment === prefix && secretObject.tags.type === type) {
-      console.log(`For this environment: ${prefix} and type: ${type} we have this ENV: ${secretObject.name} = ${secretObject.value}`);
+      console.log(`For this environment: ${prefix} and type: ${type} we have this ENV: ${secretObject.name} = ${(secretObject.value).substr(0,5)}****`);
+      core.exportVariable(secretObject.name, secretObject.value);
+      core.setSecret(secretObject.value);
     }
   })
 
 };
 
-const KEY_VAULT_URI = core.getInput('KEY_VAULT_URI');
-process.env['KEY_VAULT_URI'] = KEY_VAULT_URI;
+
 const e  = core.getInput('ENVIRONMENT');
 const t  = core.getInput('TYPE');
 
